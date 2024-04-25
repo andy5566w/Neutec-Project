bb0a31a3636a4d5991f06086364ea139487b170f
andy.tsai <andy5566w@gmail.com>
2024-04-25 17:45:32 +0800

    [Neutec-Project] add flash effect

--- src/components/Jiugongge/Jiugongge.vue
+++ src/components/Jiugongge/Jiugongge.vue
@@ -1,14 +1,6 @@
 <template>
   <div class="container">
-    <div class="box"></div>
-    <div class="box"></div>
-    <div class="box"></div>
-    <div class="box"></div>
-    <div class="box"></div>
-    <div class="box"></div>
-    <div class="box"></div>
-    <div class="box"></div>
-    <div class="box"></div>
+    <div class="box" v-for="n in 9" :key="n"></div>
   </div>
 </template>
 <script setup></script>
@@ -29,6 +21,24 @@
       rgba(113, 81, 95, 1) 81%,
       rgba(0, 0, 0, 1) 100%
     );
+
+    @for $i from 1 through 9 {
+      @if ($i == 3 or $i == 5 or $i == 9) {
+        &:nth-child(#{$i}) {
+          animation: flash 0.5s ease-in infinite alternate;
+          animation-delay: #{$i * 0.1}s;
+        }
+      }
+    }
+  }
+}
+
+@keyframes flash {
+  from {
+    opacity: 1;
+  }
+  to {
+    opacity: 0.6;
   }
 }
 </style>
