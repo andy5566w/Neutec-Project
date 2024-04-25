13198ab592e873690b98e8cd588fb5546449f504
andy.tsai <andy5566w@gmail.com>
2024-04-25 17:19:07 +0800

    [Neutec-Project]  build Jiugongge.vue component.

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,12 @@
 <template>
   <side-bar-button />
   <side-bar />
+  <jiugongge />
 </template>
 <script setup>
 import SideBarButton from '@/components/SideBar/SideBarButton.vue'
 import SideBar from '@/components/SideBar/SideBar.vue'
+import Jiugongge from '@/components/Jiugongge/Jiugongge.vue'
 </script>
 
 <style scoped></style>
---
+++ src/components/Jiugongge/Jiugongge.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="container">
+    <div class="box"></div>
+    <div class="box"></div>
+    <div class="box"></div>
+    <div class="box"></div>
+    <div class="box"></div>
+    <div class="box"></div>
+    <div class="box"></div>
+    <div class="box"></div>
+    <div class="box"></div>
+  </div>
+</template>
+<script setup></script>
+
+<style scoped lang="scss">
+.container {
+  display: grid;
+  grid-template-columns: repeat(3, 1fr);
+  gap: 10px;
+  padding: 0 10px;
+  height: 95vh;
+  align-content: end;
+  .box {
+    height: 100px;
+    border: black solid 2px;
+    background: radial-gradient(
+      circle,
+      rgba(113, 81, 95, 1) 81%,
+      rgba(0, 0, 0, 1) 100%
+    );
+  }
+}
+</style>
