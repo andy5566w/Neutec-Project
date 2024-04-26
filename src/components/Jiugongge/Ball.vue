4cd063a97d9a799d7d24d71e489ce689dbabe99f
andy.tsai <andy5566w@gmail.com>
2024-04-26 15:02:46 +0800

    [Neutec-Project] build Ball.vue component

---
+++ src/components/Jiugongge/Ball.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="ball"></div>
+</template>
+<script setup></script>
+
+<style scoped lang="scss">
+.ball {
+  width: 30px;
+  height: 30px;
+  border-radius: 50%;
+  background-color: #a5f12b;
+}
+</style>
